cd4819c4b1f29d12e057c9a6d0a908832ae39ae6
fabiloco <faalsaru@gmail.com>
2021-05-18 17:26:45 -0500

    adding logigc

--- js/game.js
+++ js/game.js
@@ -7,6 +7,11 @@ class Game{
         const MAX_PONIES = 10;
         this.allPoniesInfo = []
 
+        this.firstSelect = {node: undefined, id:0};
+        this.secondSelect = {node: undefined, id:0};
+        this.firstMovement = false;
+        this.corrects = [];
+
         this.poniesIds = this.generateRanIds(MAX_PONIES);
 
         this.suffleArray(this.poniesIds);
@@ -26,9 +31,11 @@ class Game{
         ponies.forEach(pony => {
             const card = document.createElement('div');
             card.classList.add('card');
+            card.id = pony.id;
 
             const front = document.createElement('div');
             front.classList.add('card-front');
+            //front.textContent = pony.id;
             
             const back = document.createElement('div');
             back.classList.add('card-back');
@@ -42,13 +49,51 @@ class Game{
         });
     }
 
-    addEvents(){
+
+
+    addEvents() {
         window.addEventListener('click', e => {
-            console.log(e.target);
-            if(e.target.parentNode.classList.contains("card")){
-                e.target.parentNode.classList.toggle("rotate");
-                console.log(e.target);
-                console.log(e.target.parentNode);
+            if(e.target.parentNode.classList.contains("card") && !this.corrects.includes(e.target.parentNode.id)){
+                if(!this.firstMovement) {
+                    this.firstMovement = true;
+
+                    this.firstSelect.node = e.target.parentNode;
+                    this.firstSelect.id = e.target.parentNode.id;
+
+                    console.log(this.firstSelect);
+
+                    e.target.parentNode.classList.toggle("rotate");
+                    console.log(e.target.parentNode);
+                }else {
+                    this.secondSelect.node = e.target.parentNode;
+                    this.secondSelect.id = e.target.parentNode.id;
+
+                    console.log(this.secondSelect);
+
+                    if(this.firstSelect.node === this.secondSelect.node) {
+                        console.log("same");
+                    }
+                    if(this.firstSelect.node !== this.secondSelect.node) {
+
+                        if(this.firstSelect.id === this.secondSelect.id) {
+                            console.log("Right")
+                            this.firstMovement = false;
+                            e.target.parentNode.classList.toggle("rotate");
+                            this.corrects.push(e.target.parentNode.id);
+                            console.log(this.corrects);
+                        }
+
+                        else{
+                            console.log("Wrong");
+                            this.firstMovement = false;
+                            e.target.parentNode.classList.toggle("rotate");
+                            setTimeout((e) => {
+                                this.firstSelect.node.classList.toggle('rotate');
+                                this.secondSelect.node.classList.toggle('rotate');
+                            } ,700);
+                        }
+                    }
+                }
             }
         });
     }
